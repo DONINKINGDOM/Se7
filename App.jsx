0d86c005b52427d3d4c15f89f12b6e927248d878
DONINKINGDOM <kunalsaini995362@gmail.com>
2025-06-20 13:56:24 +0530

    Initial commit - Uploaded full SE7EN Tools site

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>👑 Welcome to DONINKINGDOM</h1>
+      <p>This is the SE7EN Tools homepage. Tools and seva coming soon.</p>
+    </div>
+  );
+}
